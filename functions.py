61ccb82c5f177a022c8df914d24a70f91914a485
Pefeci <241015@vutbr.cz>
2024-04-01 20:16:57 +0200

    added sixfold

--- functions.py
+++ functions.py
@@ -122,41 +122,7 @@ def wavelet_transformation(image):
     wavelet_features = np.concatenate((cA,cH))
     return wavelet_features
 
-#geometric features?
-
-# Histogram //TODO FUUUUUUCK
-def calculate_histogram(image):
-    _, image = cv2.threshold(image, 0.5, 1, cv2.THRESH_BINARY_INV)
-    image *= 255
-    image = np.array(image, dtype=np.uint8)
-    # show_single_image(image)
-
-    #hist, bins = np.histogram(image)
-    hist = cv2.calcHist([image], [0], None, [256], [0,256])
-    #hist = hist.squeeze()
-    #hist.astype(np.int64)
-    hist /= hist.sum()
-
-    plt.plot(hist)
-    plt.show(block=True)
-
-    cdf = np.cumsum(hist)
-
-    # Plot CDF
-    plt.plot( cdf)
-    plt.title('Cumulative Distribution Function (CDF)')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Cumulative Probability')
-    plt.xlim(0, 255)
-    plt.ylim(0, 1)
-    plt.show(block=True)
-    return cdf
-
-
-
-
-
-# Count of pixels and representation of count of pixel upon y and x axis:
+# Count of pixels and representation of count of pixel upon y and x axis (histogram):
 def plot_non_white_pixels(non_white_pixels_rows, non_white_pixels_columns):
     # Plot non-white pixels in rows
     plt.figure(figsize=(10, 5))
@@ -193,8 +159,88 @@ def count_none_white_pixels(image, count_axis=False):
 
 
 # Center of mass, Normalized area ,Aspect Ratio, Tri surface feature,six fold surface feature and Transition feature
+# https://www.researchgate.net/publication/258650160_Handwritten_Signature_Verification_using_Neural_Network
+def calculate_center_of_mass(image):
+    width = image.shape[1]
+    height = image.shape[0]
+    half_width = width // 2
+    first_half = image[:half_width, :]
+    second_half = image[half_width:, :]
 
-# morphological features
+    M1 = cv2.moments(first_half)
+    M2 = cv2.moments(second_half)
+
+    center_of_mass_first_half_x = M1['m10'] / M1['m00'] if M1['m00'] != 0 else 0
+    center_of_mass_first_half_y = M1['m01'] / M1['m00'] if M1['m00'] != 0 else 0
+    center_of_mass_second_half_x = M2['m10'] / M2['m00'] if M2['m00'] != 0 else 0
+    center_of_mass_second_half_y = M2['m01'] / M2['m00'] if M2['m00'] != 0 else 0
+
+    #normalization
+    center_of_mass_first_half_normalized = np.array([center_of_mass_first_half_x / half_width, center_of_mass_first_half_y / height ])
+    center_of_mass_second_half_normalized = np.array([center_of_mass_second_half_x / half_width, center_of_mass_second_half_y / height])
+
+
+    return center_of_mass_first_half_normalized, center_of_mass_second_half_normalized
+
+def calculate_normalized_shape(image):
+    image = image[:,:,0]
+    img_area = np.sum(image == 0)
+    pixel_indieces = np.where(image == 0)
+    rows, cols = pixel_indieces
+    bounding_box_area = (max(rows) - min(rows) + 1) * (max(cols) - min(cols) + 1)
+    normalized_shape = img_area / bounding_box_area
+    return normalized_shape
+
+def calculate_aspect_ratio(image):
+    image = image[:, :, 0]
+    pixel_indieces = np.where(image == 0)
+    rows, cols = pixel_indieces
+    height = max(rows) - min(rows) + 1
+    width = max(cols) - min(cols) + 1
+    aspect_ratio = width / height
+
+    return aspect_ratio
+
+def calculate_tri_surface_area(image):
+    tri_width = image.shape[1] // 3
+    parts = [image[:, i * tri_width:(i + 1) * tri_width] for i in range(3)]
+    areas = [calculate_normalized_shape(part) for part in parts]
+    return areas
+
+
+def six_fold_surface(image):
+    image = image[:, :, 0]
+    part_width = image.shape[1] // 3
+    parts = [image[:, i * part_width:(i + 1) * part_width] for i in range(3)]
+    all_features = []
+    for part in parts:
+        features = []
+        pixel_indieces = np.where(part == 0)
+        rows, cols = pixel_indieces
+        boundingbox_width = [min(cols), max(cols)]  # width
+        boundingbox_height = [min(rows), max(rows)] #height
+        bounding_box = [boundingbox_width[1] - boundingbox_width[0], boundingbox_height[1] - boundingbox_height[0]]
+        features.append(bounding_box)
+        print(f"Width of bound {boundingbox_width}")
+        print(f"Height of bound {boundingbox_height}")
+        Mx = cv2.moments(part[boundingbox_height[0]: boundingbox_height[1],boundingbox_width[0]:boundingbox_width[1]])
+        center_of_mass_x = Mx['m10'] / Mx['m00'] if Mx['m00'] != 0 else 0
+        center_of_mass_y = Mx['m01'] / Mx['m00'] if Mx['m00'] != 0 else 0
+        print(f"Mass = {center_of_mass_x} x {center_of_mass_y}")
+        area_above_center = np.sum(part[boundingbox_height[0]:(boundingbox_height[0] + int(center_of_mass_y)), boundingbox_width[0]:boundingbox_width[1]] == 0)
+        area_bellow_center = np.sum(part[(boundingbox_height[0] + int(center_of_mass_y)):boundingbox_height[1], boundingbox_width[0]:boundingbox_width[1]] == 0)
+        print(f"Bellow {area_bellow_center} and Above {area_above_center}")
+        #show_single_image(part[boundingbox_height[0]:(boundingbox_height[0] + int( center_of_mass_y)), boundingbox_width[0]:boundingbox_width[1]])
+        #show_single_image(part[(boundingbox_height[0] + int(center_of_mass_y)):boundingbox_height[1], boundingbox_width[0]:boundingbox_width[1]])
+        center_of_mass_x = center_of_mass_x / (boundingbox_width[1] - boundingbox_width[0]) #normalized
+        center_of_mass_y = center_of_mass_y / (boundingbox_height[1] - boundingbox_height[0])
+        features.append([center_of_mass_x, center_of_mass_y])
+        features.append([area_bellow_center, area_above_center])
+        all_features.append(features)
+
+    return all_features
+
+
 
 
 
